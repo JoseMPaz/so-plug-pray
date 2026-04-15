61dfaa921ab7b50a6f58fb65176aef51840ac00c
JoseMiguelPazPortilla <jpazportilla@frba.utn.edu.ar>
2026-04-15 01:22:18 -0300

    agrego ip y puertos en los config de todos los modulos

--- utils/src/utils/hello.c
+++ utils/src/utils/hello.c
@@ -3,3 +3,43 @@
 void saludar(char* quien) {
     printf("Hola desde %s!!\n", quien);
 }
+
+int crear_socket (t_conexion tipo_conexion, const char * ip, const char * puerto)
+{
+	int conexion;
+	int err;
+
+	struct addrinfo hints;
+	struct addrinfo * server_info;
+	
+	memset ( &hints , 0 , sizeof(hints) );
+	hints.ai_family 	= AF_INET;
+	hints.ai_socktype 	= SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+		
+	if (tipo_conexion == CLIENTE)
+	{
+		err = getaddrinfo ( ip , puerto , &hints , &server_info );//Pide memoria para server_info
+		conexion = socket ( server_info->ai_family, server_info->ai_socktype , server_info->ai_protocol);
+		freeaddrinfo(server_info);//devuelve memoria de server_info
+	}
+	if (tipo_conexion == SERVIDOR)
+	{	
+		err = getaddrinfo ( NULL , puerto , &hints , &server_info );//Pide memoria para server_info
+		conexion = socket ( server_info->ai_family, server_info->ai_socktype , server_info->ai_protocol);
+		
+		err = setsockopt ( conexion , SOL_SOCKET , SO_REUSEPORT , &(int){1} , sizeof(int) );
+		err = bind ( conexion , server_info->ai_addr , server_info->ai_addrlen );
+		err = listen ( conexion , SOMAXCONN );
+		if (err == -1) 
+		{
+    		puts("No se pudo hacer listen");
+    		abort();
+		}
+		freeaddrinfo(server_info);//devuelve memoria de server_info
+	}
+
+	
+		
+	return conexion;
+}
--- utils/src/utils/hello.h
+++ utils/src/utils/hello.h
@@ -3,6 +3,24 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <commons/collections/list.h>
+#include <pthread.h>
+#include <commons/string.h>
+#include <signal.h>
+#include <semaphore.h>
+
+typedef enum
+{
+	CLIENTE = 0,
+	SERVIDOR = 1
+}t_conexion;
 
 /**
 * @brief Imprime un saludo por consola
@@ -10,5 +28,7 @@
 * @return No devuelve nada
 */
 void saludar(char* quien);
+
+int crear_socket (t_conexion tipo_conexion, const char * ip, const char * puerto);
 
 #endif
